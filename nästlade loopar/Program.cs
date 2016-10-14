766251b78b6d6bcf435560394836a415ade6b88f
Axelost <axel.osterlund1@gmail.com>
2016-10-14 18:29:19 +0200

    nästlade loopar

--- nästlade loopar/Program.cs
+++ nästlade loopar/Program.cs
@@ -10,6 +10,147 @@ namespace nästlade_loopar
     {
         static void Main(string[] args)
         {
+            /*
+             //övning 1
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    if (x%2==0)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write("o");
+                    }
+
+                }
+                Console.WriteLine();
+
+            }
+            //övning 2
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    if (x % 2 == 0)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.Write("#");
+                        }
+                        else
+                        {
+                            Console.Write("o");
+                        }
+                    }
+                    else
+                    {
+                        if (i%2==0)
+                        {
+                            Console.Write("o");
+                        }
+                        else
+                        {
+                            Console.Write("#");
+                        }
+                        
+                    }
+
+                }
+                Console.WriteLine();
+
+            }
+
+            //övning 3
+            for (int i = 10; i > 0; i--)
+            {
+                for (int x = 0; x < i; x++)
+                {
+                    Console.Write("#");
+
+                }
+                Console.WriteLine();
+
+            }
+            //övning 4
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    if (x<i)
+                    {
+                        Console.Write("-");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+                    
+
+                }
+                Console.WriteLine();
+
+            }
+            //övning 5
+            for (int i = 0; i < 7; i++)
+            {
+                for (int x  = 0; x < 7; x++)
+                {
+                    if (x==i || x+i==6)
+                    {
+                        Console.Write("x");
+
+                    }
+                    else
+                    {
+                        Console.Write("-");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            //övning 6
+            for (int i = 0; i < 7; i++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    if ((x > i || x + i >6)&&(x+i<6||x<i))
+                    {
+                        Console.Write("-");
+
+                    }
+                    else
+                    {
+                        Console.Write("x");
+                    }
+                }
+                Console.WriteLine();
+
+            } 
+            */
+            //övning 7
+            for (int i = 0; i < 9; i++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    if (x+i<4||x+i>12||x-i>4||x-i<-4)
+                    {
+                        Console.Write("-");
+
+                    }
+                    else
+                    {
+                        Console.Write("x");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
